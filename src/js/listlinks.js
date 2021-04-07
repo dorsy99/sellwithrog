534b2959cc23006331f4b459555715c7461c1f0f
dorsy99 <andrewalburydor@gmail.com>
2021-04-07 11:15:17 +1000

    list links v1.1

--- src/js/listlinks.js
+++ src/js/listlinks.js
@@ -9,10 +9,31 @@ fetch('/.netlify/functions/get-routes')
   .then(function(data) {
     //document.querySelector("#message").innerHTML = `<a href="${data.url}">${data.url}</a>`;
     console.log(data);
+    data.forEach(function(item){
+        var shortlink = JSON.parse(item);
+        addRows(shortlink.data.code, shortlink.data.destination, shortlink.created_at);
+    })
     return;
   });
 
 }
 
+function addRows(link, url, date) {
+    tabBody = document.getElementsByTagName("tbody").item(0);
+    row = document.createElement("tr");
+    slcell = document.createElement("td");
+    urlcell = document.createElement("td");
+    createdcell = document.createElement("td");
+    sldata = document.createTextNode(link);
+    urldata = document.createTextNode(url);
+    datedata = document.createTextNode(date)
+    slcell.appendChild(sldata);
+    urlcell.appendChild(urldata);
+    createdcell.appendChild(datedata);
+    row.appendChild(slcell);
+    row.appendChild(urlcell);
+    row.appendChild(datedata);
+    tabBody.appendChild(row);
+}
 
 getAllLinks();
